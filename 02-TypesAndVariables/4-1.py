0bdd4d611b8020e49a63f1c875e7adcad950c003
Vlad Bevz <vladyslav.bevz@gmail.com>
2024-10-11 10:13:17 +0200

    section 4 completed

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+print(f"My name is {name}.")
+age = 15
+height = 180
+age_next = age + 6
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age_next} years old.  ")
